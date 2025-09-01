2b4e8185b8e147a8949d2c2fdbe4cf0c3f2505f0
Flavimm <flavionegocios2004@gmail.com>
2025-08-31 23:10:01 -0300

    Adiciona módulos "Básico" e "Tipos" com exercícios de PHP
    SECAO 4 / 37 - 41

---
+++ tipos/int.php
@@ -0,0 +1,14 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11); // mostra o tipo da variável
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>'; // maior valor
+echo PHP_INT_MIN, '<br>'; // menor valor
+echo 017, '<br>'; // Base octal
+echo 0b11000111001, '<br>'; // Base binária
+echo 0x117acf0; // Base hexa
